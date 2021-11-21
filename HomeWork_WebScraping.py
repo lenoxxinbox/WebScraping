97521ded0a26745140ca773b22ed6980250d0615
lenoxxinbox <lenoxxinbox150279@gmail.com>
2021-11-21 21:53:35 +0300

    Fix HomeWork_WebScraping.py

--- HomeWork_WebScraping.py
+++ HomeWork_WebScraping.py
@@ -8,20 +8,21 @@ response.raise_for_status()
 text = response.text
 soup = BeautifulSoup(text, features='html.parser')
 
-KEYWORDS = {'дизайн', 'фото', 'web', 'Python *'}
+KEYWORDS = {'Работая', 'фото', 'web', 'Python *'}
 
 articles = soup.find_all('article')
 for article in articles:
-    hubs = article.find_all('span', class_='tm-article-snippet__hubs-item')
+    hubs = article.find_all('div', class_='article-formatted-body article-formatted-body_version-2')
+
     hubs_text = {hub.text for hub in hubs}
-
     for hub_text in hubs_text:
-        if KEYWORDS & hubs_text:
-            title = article.find('h2')
-            link = title.find('a').attrs.get('href')
-            url = 'https://habr.com' + link
-            date = article.find('time').attrs.get('title')
-            print(date)
-            print(title.text)
-            print(url)
-
+        if hub_text != set():
+            a = set(hub_text.split())
+            if KEYWORDS & a:
+                title = article.find('h2')
+                link = title.find('a').attrs.get('href')
+                url = 'https://habr.com' + link
+                date = article.find('time').attrs.get('title')
+                print(date)
+                print(title.text)
+                print(url)
